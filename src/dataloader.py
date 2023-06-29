06a0400ea1af17d96248f0b91e9ee3f9620b5f4f
wly910 <893162711@qq.com>
2023-06-30 03:05:45 +0800

    1

--- src/dataloader.py
+++ src/dataloader.py
@@ -5,9 +5,11 @@ import torchvision
 from torch.utils.data import DataLoader
 from torch.utils.tensorboard import SummaryWriter
 
-test_data = torchvision.datasets.CIFAR10("./dataset", train=False, transform=torchvision.transforms.ToTensor())
+test_data = torchvision.datasets.CIFAR10(
+    "./dataset", train=False, transform=torchvision.transforms.ToTensor())
 
-test_loader = DataLoader(dataset=test_data, batch_size=64, shuffle=True, num_workers=0, drop_last=True)
+test_loader = DataLoader(dataset=test_data, batch_size=64,
+                         shuffle=True, num_workers=0, drop_last=True)
 
 # 测试数据集中第一张图片及target
 img, target = test_data[0]
@@ -25,5 +27,3 @@ for epoch in range(2):
         step = step + 1
 
 writer.close()
-
-
